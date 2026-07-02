561b9bf5ed4230db937c59da564caf651545cec3
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-02 11:32:56 -0400

    Add: lowest-common-ancestor-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree/submission-1.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node):
+            if not node:
+                return 
+
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+
+            if node.val == p.val or node.val == q.val:
+                return node
+
+            if left and right:
+                return node
+
+            return left if left else right
+
+        return dfs(root)
+
+    
+
+        
