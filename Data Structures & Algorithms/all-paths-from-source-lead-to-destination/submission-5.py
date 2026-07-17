a1bc753f2de0208d659f29c73729a953ffbcefe0
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-17 13:08:47 -0400

    Add: all-paths-from-source-lead-to-destination - submission-5

---
+++ Data Structures & Algorithms/all-paths-from-source-lead-to-destination/submission-5.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+        for src, dest in edges:
+            graph[src].append(dest)
+
+        visited = set()
+        def dfs(node):
+            if len(graph[node]) == 0:
+                return node == destination
+            
+            # detect a cylce
+            if node in visited:
+                return False
+
+            visited.add(node)
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            visited.discard(node)
+            return True
+        return dfs(source)
+        
+
+
+         
