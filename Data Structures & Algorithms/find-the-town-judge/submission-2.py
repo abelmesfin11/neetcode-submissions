a849959a7fa806a11e3ded0714a7fcc24209c3c7
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-17 13:53:07 -0400

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for x, y in trust:
+            graph[x].append(y)
+        
+
+        for i in range(1, n + 1):
+            out = len(graph[i])
+            inn = 0
+            for j in range(1, n+1):
+                if i != j and i in graph[j]:
+                    inn += 1
+
+            if inn == n - 1 and out == 0:
+                return i
+
+        return -1
+
+
+
+        
+
+        
+    
+       
+   
+
+    
+        
