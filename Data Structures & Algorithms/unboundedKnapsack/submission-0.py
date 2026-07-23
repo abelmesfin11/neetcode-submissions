1d8b4a4305d3baadd2c6ce3d34fd60ef5f6ccce1
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-23 10:18:12 -0400

    Add: unboundedKnapsack - submission-0

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        memo = {}
+        def dp(i, curr):
+            if (i, curr) in memo:
+                return memo[(i, curr)]
+
+            if i == len(profit):
+                return 0
+
+            if curr + weight[i] <= capacity:
+                memo[(i, curr)] = max(dp(i+1, curr), profit[i] + dp(i, curr + weight[i]))
+            else:
+                memo[(i, curr)] = dp(i + 1, curr)
+
+            return memo[(i, curr)] 
+
+        return dp(0, 0)
+
+
+
+            
+            
