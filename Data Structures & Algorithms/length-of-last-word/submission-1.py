cab9df99db130e78f75ea1f2289ceac6b1764031
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-27 10:46:41 -0400

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == " ":
+                continue
+            else:
+                count += 1
+                if s[i - 1] == " ":
+                    break
+        return count
+            
+       
