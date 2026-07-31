969ef3561cf7ec9937ba9725db157fc3fe508213
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-07-31 14:11:09 -0400

    Add: confusing-number - submission-9

---
+++ Data Structures & Algorithms/confusing-number/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        if n == 0: 
+            return False
+        invert_map = {"0":"0", "1":"1", "8":"8", "6":"9", "9":"6"}
+        rotated_number = []
+        curr = n
+        while curr:
+            last = str(curr % 10)
+            if last in invert_map:
+                rotated_number.append(invert_map[last])
+            else:
+                return False
+            curr = curr // 10
+        num = "".join(rotated_number)
+        return n != int(num)
