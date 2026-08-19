7e4ee7f0f15be85ba5172a502f27899a2d8f90d9
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-08-18 20:25:41 -0400

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.py
@@ -0,0 +1,15 @@
+import bisect
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums, target)
+        # l, r = 0, len(nums)
+        # while l < r:
+        #     m = (l + r) // 2
+        #     if nums[m] >= target:
+        #         r = m
+        #     else:
+        #         l = m + 1
+        # return l
+
+        
