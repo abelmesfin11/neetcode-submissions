d2f7b3aa39f2758f81c2146c3be9cae1f88c9451
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-08-21 18:52:52 -0400

    Add: basic-calculator-ii - submission-2

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-2.py
@@ -0,0 +1,35 @@
+import math
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        op = '+'
+        s = s.replace(' ', '')
+
+        for idx, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            if not ch.isdigit() or idx == len(s) - 1:
+                if op == "+":
+                    stack.append(num)
+
+                elif op == "-":
+                    stack.append(-num)
+
+                elif op == "*":
+                    stack.append(stack.pop() * num)
+
+                else:
+                    stack.append(int(stack.pop() / num))
+
+                op = ch
+                num = 0
+
+        return sum(stack)
+
+                
+
+
+        
