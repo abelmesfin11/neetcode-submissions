faa7724e39d69581bebf1abc55ca3ffbdbdf6cf3
Abel Mesfin <119023930+abelmesfin11@users.noreply.github.com>
2026-08-23 12:14:14 -0400

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        board = [["."] * n for i in range(n)]
+
+        def back(r):
+            if r == n:
+                ans.append(["".join(row) for row in board])
+                return
+
+            for c in range(n):
+                if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                back(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        back(0)
+        return ans
+
+
+
+
+
+        
+        
